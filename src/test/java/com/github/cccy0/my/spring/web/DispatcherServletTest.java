c01f121a4ac71d37894105fae9494771161ce477
cccy0 <cccy0@qq.com>
2022-07-28 12:16:35 +0800

    change directory structure

--- src/test/java/com/github/cccy0/my/spring/web/DispatcherServletTest.java
+++ src/test/java/com/github/cccy0/my/spring/web/DispatcherServletTest.java
@@ -1,5 +1,6 @@
 package com.github.cccy0.my.spring.web;
 
+import com.github.cccy0.my.spring.web.DispatcherServlet;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static org.junit.Assert.*;
 
@@ -43,13 +46,16 @@ public class DispatcherServletTest {
     }
 
     @Test
-    public void doGet() throws ServletException, IOException {
-
-        dispatcherServlet.doGet(httpServletRequest, httpServletResponse);
+    public void doGet() throws ServletException, IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = DispatcherServlet.class.getDeclaredMethod("doGet", HttpServletRequest.class, HttpServletResponse.class);
+        method.setAccessible(true);
+        method.invoke(dispatcherServlet, httpServletRequest, httpServletResponse);
     }
 
     @Test
-    public void doPost() throws ServletException, IOException {
-        dispatcherServlet.doPost(httpServletRequest, httpServletResponse);
+    public void doPost() throws ServletException, IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = DispatcherServlet.class.getDeclaredMethod("doPost", HttpServletRequest.class, HttpServletResponse.class);
+        method.setAccessible(true);
+        method.invoke(dispatcherServlet, httpServletRequest, httpServletResponse);
     }
 }
